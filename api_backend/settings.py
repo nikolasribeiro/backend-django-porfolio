f7d57a7ceab6e3fbc26f270a4b7981c08e10aa8f
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-03-30 18:38:01 -0300

    delete runtime file

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -133,11 +133,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    'img/porfolio/',
+    os.path.join(BASE_DIR, 'static'),
 ]
  
 
