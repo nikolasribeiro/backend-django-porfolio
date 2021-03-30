79cc203d9aae5fee795e717a00aac223f57e4e92
Nicolas Ribeiro <nikolasribeiro2@outlook.com>
2021-03-30 18:33:06 -0300

    delete runtime file

--- api_backend/settings.py
+++ api_backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("PORFOLIO_API_BACKEND_KEY")
-
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
